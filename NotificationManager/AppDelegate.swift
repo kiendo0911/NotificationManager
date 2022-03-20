58944c61a9d5d006c8407a91c0b6086db5bfcda8
kiendo0911 <33291661+kiendo0911@users.noreply.github.com>
2022-03-20 15:44:07 +0700

    Update UITests and UnitTest
    + update UnitTests and UITests version deployment target
    + remove Main.storyboard

--- NotificationManager/AppDelegate.swift
+++ NotificationManager/AppDelegate.swift
@@ -11,10 +11,13 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow()
+        window?.rootViewController = NotificationsViewController(nibName: "NotificationsViewController", bundle: nil)
+        window?.makeKeyAndVisible()
         return true
     }
 
